611390a5909c2c9cc866d12e5fae9bf948d094d0
developerSaad <slh098098@gmail.com>
2023-01-07 18:45:07 +0500

    Added home section, minor tweaks to the header.
    Done with the home section. Its consists of an epic slider for which I used https://swiperjs.com/ . Got the pics from google.

--- index.js
+++ index.js
@@ -25,3 +25,18 @@ closeSearch.addEventListener("click", ()=>{
     searchForm.classList.remove('active')
 })
 
+
+
+var swiper = new Swiper(".home-slider", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 7500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    loop: true,
+  });
