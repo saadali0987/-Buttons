4c7bfdc809fd194ba28e97921d648c705179c489
developerSaad <slh098098@gmail.com>
2023-01-09 22:42:33 +0500

    project completed

--- index.js
+++ index.js
@@ -27,16 +27,26 @@ closeSearch.addEventListener("click", ()=>{
 
 
 
-var swiper = new Swiper(".home-slider", {
+var swiper = new Swiper(".review-slider", {
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
-      delay: 7500,
+      delay: 5000,
       disableOnInteraction: false,
     },
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
-    },
     loop: true,
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+      },
+      640:{
+        slidesPerView:2,
+      },
+      768:{
+        slidesPerView:2,
+      },
+      1024:{
+        slidesPerView:2
+      }
+    }
   });
